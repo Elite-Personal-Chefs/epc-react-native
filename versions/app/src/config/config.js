dd3401845c65a79e7e62d92217df2a8336dc8fed
Joshua A.L. Harrison <joshua@arcyadele.com>
2022-11-09 11:23:10 -0600

    Update to use EAS and add menu templates

--- versions/app/src/config/config.js
+++ versions/app/src/config/config.js
@@ -6,17 +6,17 @@ import '@firebase/storage';
 
 
 const configKeys = {
-    apiKey: "AIzaSyD7rHwMdVtWfdhTGGH0fTQ1Xhk4RjRwfZI",
-    authDomain: "elite-ee4b7.firebaseapp.com",
-    projectId: "elite-ee4b7",
-    storageBucket: "elite-ee4b7.appspot.com",
-    messagingSenderId: "37474980291",
-    appId: "1:37474980291:web:63150f372b3e92267dad76",
-    measurementId: "G-WSZ014RCDH",
-    databaseURL: "https://elite-ee4b7.firebaseio.com",
-    api_local: "http://localhost:5001/elite-ee4b7/us-central1/api/v1/",
-    api_dev: "http://localhost:5001/elite-ee4b7/us-central1/api/v1/",
-    api_live: "https://us-central1-elite-ee4b7.cloudfunctions.net/api/v1/"
+	apiKey: "AIzaSyD7rHwMdVtWfdhTGGH0fTQ1Xhk4RjRwfZI",
+	authDomain: "elite-ee4b7.firebaseapp.com",
+	projectId: "elite-ee4b7",
+	storageBucket: "elite-ee4b7.appspot.com",
+	messagingSenderId: "37474980291",
+	appId: "1:37474980291:web:63150f372b3e92267dad76",
+	measurementId: "G-WSZ014RCDH",
+	databaseURL: "https://elite-ee4b7.firebaseio.com",
+	api_local: "http://localhost:8080/v1/",
+	api_dev: "https://elite-personal-chefs-server.onrender.com/v1/",
+	api_live: "https://elite-personal-chefs-server.onrender.com/v1/",
 };
 
 if (!firebase.apps.length) {
