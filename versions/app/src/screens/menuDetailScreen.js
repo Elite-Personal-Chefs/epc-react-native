d0327bd512f9b13a1995cd4ebdebc4bef0808845
Armando Arteaga <hey@armandohimself.com>
2022-11-15 13:49:01 -0600

    Change Request: Menu Price Flag

--- versions/app/src/screens/menuDetailScreen.js
+++ versions/app/src/screens/menuDetailScreen.js
@@ -34,272 +34,228 @@ import Theme from "../styles/theme.style.js";
 /*******************************************************************************/
 
 export default function MenuDetailScreen({ route, navigation }) {
-	const appsGlobalContext = useContext(AppContext);
-	const uid = appsGlobalContext.userID;
-	const activeFlow = appsGlobalContext.activeFlow;
-	console.log(activeFlow);
-	console.log(uid);
-	const [isEditable, setIsEditable] = useState(false);
-	const [menuItems, setMenuItems] = useState(false);
-	const pageName = route.params.pageName;
-	const details = route.params.details;
-	const [menuImg, setMenuImg] = useState(null);
-	const [added, setAdded] = useState(false);
-	const [adding, setAdding] = useState(false);
+  const appsGlobalContext = useContext(AppContext);
+  const uid = appsGlobalContext.userID;
+  const activeFlow = appsGlobalContext.activeFlow;
+  console.log(activeFlow);
+  console.log(uid);
+  const [isEditable, setIsEditable] = useState(false);
+  const [menuItems, setMenuItems] = useState(false);
+  const pageName = route.params.pageName;
+  const details = route.params.details;
+  const [knownMenuPrice, setKnownMenuPrice] = useState(
+    details.price ? Number.parseInt(details.price) : false
+  );
+  const [menuImg, setMenuImg] = useState(null);
+  const [added, setAdded] = useState(false);
+  const [adding, setAdding] = useState(false);
 
-	const addToMyMenu = async (menuID) => {
-        setAdding(true);
-		console.log(menuID, uid);
-		try {
-			const result = await fetch(
-				getEndpoint(appsGlobalContext, "copy_template"),
-				{
-					method: "POST",
-					headers: {
-						Accept: "application/json",
-						"Content-Type": "application/json",
-					},
-					body: JSON.stringify({
-						menu_template_id: menuID,
-						user_id: uid,
-						add_data: {
-							is_custom: false,
-							is_published: false,
-							menu_template_id: menuID,
-						},
-					}),
-				}
-			);
-			const json = await result.json();
-            setAdding(false);
-			setAdded(true);
-		} catch (error) {
-			console.log(error);
-		}
-	};
+  const addToMyMenu = async (menuID) => {
+    setAdding(true);
+    console.log(menuID, uid);
+    try {
+      const result = await fetch(getEndpoint(appsGlobalContext, "copy_template"), {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          menu_template_id: menuID,
+          user_id: uid,
+          add_data: {
+            is_custom: false,
+            is_published: false,
+            menu_template_id: menuID,
+          },
+        }),
+      });
+      const json = await result.json();
+      setAdding(false);
+      setAdded(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-	const editMenu = () => {
-		let editableMenuFormat = [];
-		menuItems.map((menu, index) => {
-			let addCourse = {
-				type: "course",
-				title: menu.course,
-			};
-			editableMenuFormat.push(addCourse);
-			menu.items.map((item) => {
-				let addItem = {
-					type: "item",
-					title: item.title,
-					description: item.description ? item.description : "",
-					course: item.course,
-				};
-				editableMenuFormat.push(addItem);
-			});
-		});
-		console.log(details);
-		navigation.navigate("Create Menu", {
-			description: details.description,
-			menu: editableMenuFormat,
-			menuID: details.id,
-			name: details.title,
-			photo: menuImg, //details.photos[0],
-			price: details.price,
-		});
-	};
+  const editMenu = () => {
+    let editableMenuFormat = [];
+    menuItems.map((menu, index) => {
+      let addCourse = {
+        type: "course",
+        title: menu.course,
+      };
+      editableMenuFormat.push(addCourse);
+      menu.items.map((item) => {
+        let addItem = {
+          type: "item",
+          title: item.title,
+          description: item.description ? item.description : "",
+          course: item.course,
+        };
+        editableMenuFormat.push(addItem);
+      });
+    });
+    console.log(details);
+    navigation.navigate("Create Menu", {
+      description: details.description,
+      menu: editableMenuFormat,
+      menuID: details.id,
+      name: details.title,
+      photo: menuImg, //details.photos[0],
+      price: details.price,
+    });
+  };
 
-	const getMenus = async (details) => {
-		//console.log(uid, details);
-		//If a chefID is passed in the details then use that as the ID of the chef to look for
-		const chefID = details.chefID ? details.chefID : uid;
-		//If this is a template page look for the menu in templates
-		//otherwise look into the chefs colelction of menus
-		const firestore = firebase.firestore();
-		const menuRef =
-			pageName == "Templates"
-				? firestore.collection("menu_templates").doc(details.id)
-				: firestore
-						.collection("chefs")
-						.doc(chefID)
-						.collection("menus")
-						.doc(details.id);
-		const menuDoc = await menuRef.get();
-		if (!menuDoc.exists) {
-			console.log("No menu found");
-		} else {
-			let menu = menuDoc.data();
-			let courses = menu.courses;
-			let menuItems = [];
-			for await (const course of courses) {
-				//Get all courses for this menu
-				let courseSnapshot = await menuRef.collection(course).get();
-				if (!courseSnapshot.empty) {
-					let items = [];
-					courseSnapshot.forEach((doc) => {
-						let item = doc.data();
-						items.push(item);
-					});
-					menuItems.push({ items, course: course });
-				}
-			}
-			//Set menu image if one exists
-			if (menu.photos) {
-				console.log("FOUND IT");
-				setMenuImg({ uri: menu.photos.pop() });
-				//useState(require('../assets/food_pasta.png'))
-			} else {
-				setMenuImg(require("../assets/food_pasta.png"));
-			}
-			setMenuItems(menuItems);
-		}
-	};
+  const getMenus = async (details) => {
+    //console.log(uid, details);
+    //If a chefID is passed in the details then use that as the ID of the chef to look for
+    const chefID = details.chefID ? details.chefID : uid;
+    //If this is a template page look for the menu in templates
+    //otherwise look into the chefs colelction of menus
+    const firestore = firebase.firestore();
+    const menuRef =
+      pageName == "Templates"
+        ? firestore.collection("menu_templates").doc(details.id)
+        : firestore.collection("chefs").doc(chefID).collection("menus").doc(details.id);
+    const menuDoc = await menuRef.get();
+    if (!menuDoc.exists) {
+      console.log("No menu found");
+    } else {
+      let menu = menuDoc.data();
+      let courses = menu.courses;
+      let menuItems = [];
+      for await (const course of courses) {
+        //Get all courses for this menu
+        let courseSnapshot = await menuRef.collection(course).get();
+        if (!courseSnapshot.empty) {
+          let items = [];
+          courseSnapshot.forEach((doc) => {
+            let item = doc.data();
+            items.push(item);
+          });
+          menuItems.push({ items, course: course });
+        }
+      }
+      //Set menu image if one exists
+      if (menu.photos) {
+        console.log("FOUND IT");
+        setMenuImg({ uri: menu.photos.pop() });
+        //useState(require('../assets/food_pasta.png'))
+      } else {
+        setMenuImg(require("../assets/food_pasta.png"));
+      }
+      setMenuItems(menuItems);
+    }
+  };
 
-	useEffect(() => {
-		console.log("getting details formenu screen");
-		console.log("ACTIVE FLOW", appsGlobalContext.activeFlow);
-		getMenus(details);
-	}, []);
+  useEffect(() => {
+    console.log("getting details formenu screen");
+    console.log("ACTIVE FLOW", appsGlobalContext.activeFlow);
+    getMenus(details);
+  }, []);
 
-	return (
-		<SafeAreaView style={globalStyles.safe_light}>
-			<ScrollView
-				showsVerticalScrollIndicator={false}
-				style={{ width: "100%", flex: 1 }}
-			>
-				{menuImg ? (
-					<Image source={menuImg} style={styles.image} />
-				) : (
-					<View style={styles.container}>
-						<ActivityIndicator
-							size="small"
-							color={Theme.SECONDARY_COLOR}
-							style={styles.image}
-						/>
-					</View>
-				)}
-				<View style={styles.content}>
-					<View style={styles.header}>
-						<View style={styles.title}>
-								<Text style={globalStyles.h1}>{details.title}</Text>
-						</View>
-						<View style={styles.price_cont}>
-								<Text style={styles.price_label}>EPC Suggested Price</Text>
-								<Text style={styles.price}>${details.price}<Text style={styles.price_label}>/Person</Text></Text>
-						</View>
-					</View>
-					{pageName == "Templates" && (
-						<View style={styles.btn_cont}>
-							{added ? (
-								<CustomButton
-									text="Added to My Menus"
-									size="big"
-									disabled="true"
-									checkmark="true"
-								/>
-							) : (
-								<CustomButton
-									text={adding ? "Adding..." : "Add to My Menus"}
-									onPress={() => addToMyMenu(details.id)}
-									size="big"
-                                    disabled={adding}
-								/>
-							)}
-						</View>
-					)}
-					{pageName == "Your Menus" && activeFlow == "chefs" && (
-						<View style={styles.btn_cont}>
-							<CustomButton
-								text="Edit"
-								onPress={() => editMenu(true)}
-								size="big"
-							/>
-						</View>
-					)}
-					<View style={[globalStyles.card, { width: "100%" }]}>
-						<View style={globalStyles.card_header}>
-							<Text style={globalStyles.h3}>Description</Text>
-						</View>
-						<Text style={globalStyles.card_content}>
-							{details.description}
-						</Text>
-					</View>
-					<View style={[globalStyles.card, { width: "100%" }]}>
-						{menuItems ? (
-							menuItems.map((menu, index) => {
-								return (
-									<View
-										style={menusStyles.menu_course_cont}
-										key={index}
-									>
-										{isEditable ? (
-											<TextInput
-												style={[styles.custom_input]}
-												placeholder="Course"
-												placeholderTextColor={
-													Theme.FAINT
-												}
-												keyboardType="default"
-												//onChangeText={(text) => changeEmail(text)}
-												//value={(menu.course) ? menu.course : newCourse}
-												value="HI"
-												underlineColorAndroid="transparent"
-												autoCapitalize="none"
-												//onFocus={() => setFocusField("email")}
-												//onBlur={() => setFocusField(null)}
-												//setFocus={focusField}
-											/>
-										) : (
-											menu.course != "blank" && (
-												<Text
-													style={
-														menusStyles.menu_course
-													}
-												>
-													- {menu.course} -
-												</Text>
-											)
-										)}
-										{menu.items.map((item, index2) => {
-											return (
-												<View
-													style={
-														menusStyles.menu_item_cont
-													}
-													key={index2}
-												>
-													<Text
-														style={
-															menusStyles.menu_name
-														}
-													>
-														{item.title ||
-															item.item_name}
-													</Text>
-													{item.description != "" && (
-														<Text
-															style={
-																menusStyles.menu_desc
-															}
-														>
-															{item.description}
-														</Text>
-													)}
-												</View>
-											);
-										})}
-									</View>
-								);
-							})
-						) : (
-							<ActivityIndicator
-								size="large"
-								color={Theme.SECONDARY_COLOR}
-							/>
-						)}
-					</View>
-				</View>
-			</ScrollView>
-		</SafeAreaView>
-	);
+  return (
+    <SafeAreaView style={globalStyles.safe_light}>
+      <ScrollView showsVerticalScrollIndicator={false} style={{ width: "100%", flex: 1 }}>
+        {menuImg ? (
+          <Image source={menuImg} style={styles.image} />
+        ) : (
+          <View style={styles.container}>
+            <ActivityIndicator size='small' color={Theme.SECONDARY_COLOR} style={styles.image} />
+          </View>
+        )}
+        <View style={styles.content}>
+          <View style={styles.header}>
+            <View style={styles.title}>
+              <Text style={globalStyles.h1}>{details.title}</Text>
+            </View>
+            {knownMenuPrice && (
+              <View style={styles.price_cont}>
+                <Text style={styles.price_label}>Suggested Price</Text>
+                <Text style={styles.price}>
+                  ${details.price}
+                  <Text style={styles.price_label}>/Person</Text>
+                </Text>
+              </View>
+            )}
+          </View>
+          {pageName == "Templates" && (
+            <View style={styles.btn_cont}>
+              {added ? (
+                <CustomButton
+                  text='Added to My Menus'
+                  size='big'
+                  disabled='true'
+                  checkmark='true'
+                />
+              ) : (
+                <CustomButton
+                  text={adding ? "Adding..." : "Add to My Menus"}
+                  onPress={() => addToMyMenu(details.id)}
+                  size='big'
+                  disabled={adding}
+                />
+              )}
+            </View>
+          )}
+          {pageName == "Your Menus" && activeFlow == "chefs" && (
+            <View style={styles.btn_cont}>
+              <CustomButton text='Edit' onPress={() => editMenu(true)} size='big' />
+            </View>
+          )}
+          <View style={[globalStyles.card, { width: "100%" }]}>
+            <View style={globalStyles.card_header}>
+              <Text style={globalStyles.h3}>Description</Text>
+            </View>
+            <Text style={globalStyles.card_content}>{details.description}</Text>
+          </View>
+          <View style={[globalStyles.card, { width: "100%" }]}>
+            {menuItems ? (
+              menuItems.map((menu, index) => {
+                return (
+                  <View style={menusStyles.menu_course_cont} key={index}>
+                    {isEditable ? (
+                      <TextInput
+                        style={[styles.custom_input]}
+                        placeholder='Course'
+                        placeholderTextColor={Theme.FAINT}
+                        keyboardType='default'
+                        //onChangeText={(text) => changeEmail(text)}
+                        //value={(menu.course) ? menu.course : newCourse}
+                        value='HI'
+                        underlineColorAndroid='transparent'
+                        autoCapitalize='none'
+                        //onFocus={() => setFocusField("email")}
+                        //onBlur={() => setFocusField(null)}
+                        //setFocus={focusField}
+                      />
+                    ) : (
+                      menu.course != "blank" && (
+                        <Text style={menusStyles.menu_course}>- {menu.course} -</Text>
+                      )
+                    )}
+                    {menu.items.map((item, index2) => {
+                      return (
+                        <View style={menusStyles.menu_item_cont} key={index2}>
+                          <Text style={menusStyles.menu_name}>{item.title || item.item_name}</Text>
+                          {item.description != "" && (
+                            <Text style={menusStyles.menu_desc}>{item.description}</Text>
+                          )}
+                        </View>
+                      );
+                    })}
+                  </View>
+                );
+              })
+            ) : (
+              <ActivityIndicator size='large' color={Theme.SECONDARY_COLOR} />
+            )}
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
